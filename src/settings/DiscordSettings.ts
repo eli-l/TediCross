36f9e54544af69889e1a6ba7858645433907b387
Lala Sabathil <aiko@aitsys.dev>
2023-03-29 09:14:32 +0200

    Update src/settings/DiscordSettings.ts

--- src/settings/DiscordSettings.ts
+++ src/settings/DiscordSettings.ts
@@ -142,7 +142,7 @@ export class DiscordSettings {
 			skipOldMessages: true,
 			useNickname: false,
 			enablePlayingStatus: false,
-			usePlayingStatusMessage: ""
+			usePlayingStatusMessage: "TediCross"
 		};
 	}
 }
